09396c1a80f5ecaa0b3cd13368cd01118c8f8394
Dustin Kane <dustinpkane@gmail.com>
2016-02-10 00:25:21 -0800

    Actually made huge progress. We get stringlits and tags!

--- analyzer.js
+++ analyzer.js
@@ -1,14 +1,38 @@
 module.exports = (data) => {
-    var tokens = {};
+    var tokens = [];
 
-    // var position = 0,
-    //     // I don't trust javascript to optimize this and not call length every time
-    //     dataLength = length(data);
-    // while(position < dataLength){
-        
-    // }
+    var position = 0,
+        // I don't trust javascript to optimize this and not call length every time
+        dataLength = data.length;
 
-    return {
-        "test" : "test"
-    };
+    while(position < dataLength){
+        var truncData = data.slice(position),
+            matchData;
+        // stringlit
+        // Start (^), then either '(not ' OR \')*' OR "(not " OR \")"
+        if ( matchData = /^('([^'\\]|(\\''))*'|"([^"\\]|(\\"))*")/.exec(truncData) ) {
+            tokens.push({
+                "type": "stringlit",
+                "text": matchData[0]
+            });
+            position += matchData[0].length;
+        }
+        // Some tag
+        else if (matchData = /^[a-zA-Z_][a-zA-Z0-9-_.]*/.exec(truncData)){
+            tokens.push({
+                "type": "tag",
+                "text": matchData[0]
+            });
+            position += matchData[0].length;
+        }
+        else if (matchData = /^[\s]+/.exec(truncData)) {
+            position += matchData[0].length;
+        }
+        else {
+            console.log("Something weird happened");
+            return -1;
+        }
+    }
+
+    return tokens;
 }
