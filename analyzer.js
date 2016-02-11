15ae31da1d8f0b09215f385366666441ce905b07
Dustin Kane <dustinpkane@gmail.com>
2016-02-10 23:08:23 -0800

    Added a big microsyntax comment

--- analyzer.js
+++ analyzer.js
@@ -1,3 +1,26 @@
+/*
+    Microsyntax
+    token     | match
+    --------------------------
+    intlit    | \d+
+    floatlit  | (\.\d+|\d+(\.\d+)?)([Ee]\d+)?
+    stringlit | ('([^'\\]|(\\''))*'|"([^"\\]|(\\"))*")
+    boollit   | (true)|(false)
+    id        | @[\p{L}$_-]+
+    relop     | <|>|(<=)|(==)|(\!=)|(>=)
+    addop     | \+|-
+    multop    | \*|\/
+    prefixop  | \!|-
+    newline   | (\r\n|\r|\n)+
+    indent    | complicated
+    dedent    | also complicated
+    keyword   | (if)|(else)|(for)|(in)|(css)|(js)
+    widget    | >.+?[\r\n]+
+    template  | \|.+?[\r\n]+
+    label     | \[[\p{L}_-]\]
+*/
+
+
 module.exports = (data) => {
 
     var tokens = [],
