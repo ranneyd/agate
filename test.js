f78a71dac8096a90dacfac9417101281ede57925
Dustin Kane <dustinpkane@gmail.com>
2016-02-14 18:25:22 -0800

    Numerous testing changes
    Made file reading synchronous
    Added reporting when produced token list is longer than expectation

--- test.js
+++ test.js
@@ -25,47 +25,48 @@ fs.readdir( dir, function ( dir_err, list ) {
         // if it matches something, this will have two elements, the first is the whole string and
         // the second is the first matching group. If regex didn't match, it's null
         if ( fileParts ){
-            fs.readFile( dir + "/" + file, 'utf8', function ( test_err, testee ) {
-                if ( test_err ) {
-                    console.log( test_err );
-                    return;
-                }
-                // For every agate file there should be a corresponding json with the expected
-                // lexical analysis
-                var expected = require( dir + "/" + fileParts[1] + ".json" );
-                var analysis = analyzer( testee );
-                if ( analysis.status === "error" && !expected.status ) {
-                    console.log("Error in Test " + fileParts[1]);
-                    console.log(analysis);
-                }
-                else if ( expected ){
-                    if ( JSON.stringify( expected ) === JSON.stringify( analysis ) ) {
-                        console.log( "Test " + fileParts[1] + " passed!" );
-                    }
-                    else {
-                        console.log( "Test " + fileParts[1] + " FAILED!" );
-
-                        var i;
-                        for(i = 0; i < expected.length; ++i) {
-                            if (JSON.stringify(expected[i]) !== JSON.stringify(analysis[i])){
-                                console.log("Item " + (i + 1))
-                                console.log("Expected:");
-                                console.log(expected[i]);
-                                console.log("Produced:");
-                                console.log(analysis[i]);
-                            }
-                        }
-
-                        // console.log( "Expected: " );
-                        // console.log( expected );
-                        // console.log( "Produced: " );
-                        // console.log(analysis);
-                    }
+            var testee = fs.readFileSync( dir + "/" + file, 'utf8');
+            // For every agate file there should be a corresponding json with the expected
+            // lexical analysis
+            var expected = require( dir + "/" + fileParts[1] + ".json" );
+            var analysis = analyzer( testee );
+            if ( analysis.status === "error" && !expected.status ) {
+                console.log("Error in Test " + fileParts[1]);
+                console.log(analysis);
+            }
+            else if ( expected ){
+                if ( JSON.stringify( expected ) === JSON.stringify( analysis ) ) {
+                    console.log( "Test " + fileParts[1] + " passed!" );
                 }
                 else {
-                    console.log( "ERROR. " + file + " found but no corresponding JSON found." );
+                    console.log( "Test " + fileParts[1] + " FAILED!" );
+
+                    var i;
+                    for(i = 0; i < expected.length; ++i) {
+                        if (JSON.stringify(expected[i]) !== JSON.stringify(analysis[i])){
+                            console.log("Item " + (i + 1))
+                            console.log("Expected:");
+                            console.log(expected[i]);
+                            console.log("Produced:");
+                            console.log(analysis[i]);
+                        }
+                    }
+                    if (expected.length < analysis.length ){
+                        console.log("Test produced additional tokens: ")
+                        for(; i < analysis.length; ++i){
+                            console.log(analysis[i]);
+                        }
+                    }
+
+                    console.log( "Expected: " );
+                    console.log( expected );
+                    console.log( "Produced: " );
+                    console.log(analysis);
                 }
-            });
+            }
+            else {
+                console.log( "ERROR. " + file + " found but no corresponding JSON found." );
+            }
         }
     });
 });
