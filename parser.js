75dd783be2b1fe8bed17573e442d71a9ec9ae9bc
Dustin Kane <dustinpkane@gmail.com>
2016-02-17 11:08:47 -0800

    Working on macrosyntax

--- parser.js
+++ parser.js
@@ -1,4 +1,39 @@
+/*
+    Macrosyntax
+    name        | def
+    ---------------------------------
+    Program     | Block
+    Block       | (Statement newline)+
+    Statement   | Element
+                | Exp
+                | Control
+                | comment
+    Element     | Tag(Class)?(Id)?Attrs?(ChildBlock?|Event ChildBlock)
+    Attrs       | openParen ((Attr Exp)|Class|Id)+ closeParen
+    ChildBlock  | indent Block dedent
+    Tag         | bareword
+    Class       | dot bareword
+    Id          | hash bareword
+    Attr        | bareword
+    Exp         | Exp1 (boolop Exp1)*
+    Exp1        | Exp2 (relop Exp2)*
+    Exp2        | Exp3 (addop Exp3)*
+    Exp3        | Exp4 (multop Exp4)*
+    Exp4        | prefixop? Exp5
+    Exp5        | Val | openParen Exp closeParen
+    Val         | ElemAttr | Lit | id | FuncCall
+    Lit         | stringlit | intlit | floatlit | boollit
+    ElemAttr    | Id?tilde Attr
+    Event       | tilde("on")?bareword
+    FuncCall    | bareword(Exp|openParen(Exp)*closeParen)
+    Control*    | If | For | While
+    If          | "if" Exp ChildBlock ("else" "if" Exp ChildBlock)*("else" ChildBlock)?
+    For         | 
+*/
+
 module.exports = (tokens) => {
+
+
     return {
         status: "success"
     };
