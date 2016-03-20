c139a9573c3cf6d330a82d1f529a58b0121489b1
Dustin Kane <dustinpkane@gmail.com>
2016-03-19 17:35:31 -0700

    doing testing in mocha like a patrician

---
+++ test/test.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// Error = require("../error.js");
+
+// var error = new Error();
+
+var scanner = require("../scanner.js");
+var parser  = require("../parser.js");
+
+var assert = require('assert');
+describe('Scanner', function() {
+  describe('literals', function () {
+    it('should recognize intlits', function () {
+        let out = scanner("5", null);
+        assert.deepStrictEqual("intlit", out[0].type);
+        assert.deepStrictEqual("5", out[0].text);
+        
+        out = scanner("-5", null);
+        assert.deepStrictEqual("minus", out[0].type);
+        assert.deepStrictEqual("intlit", out[1].type);
+        assert.deepStrictEqual("5", out[1].text);
+    });
+    it('should recognize floatlits', function () {
+        let out = scanner("3.14", null);
+        assert.deepStrictEqual("floatlit", out[0].type);
+        assert.deepStrictEqual("3.14", out[0].text);
+        
+        out = scanner("-3.14", null);
+        assert.deepStrictEqual("minus", out[0].type);
+        assert.deepStrictEqual("floatlit", out[1].type);
+        assert.deepStrictEqual("3.14", out[1].text);
+    });
+  });
+});
