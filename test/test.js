c5bb04c90687a7004340c4b1faa07a315352edc3
Dustin Kane <dustinpkane@gmail.com>
2016-03-21 12:02:56 -0700

    unit tests are going to take a thousand years

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var scanner = require("../scanner.js");
 var parser  = require("../parser.js");
 
 var assert = require('assert');
-describe('Scanner', function() {
+describe('Scanner Basics', function() {
   describe('literals', function () {
     it('should recognize intlits', function () {
         let out = scanner("5", null);
@@ -26,6 +26,36 @@ describe('Scanner', function() {
         assert.deepStrictEqual("minus", out[0].type);
         assert.deepStrictEqual("floatlit", out[1].type);
         assert.deepStrictEqual("3.14", out[1].text);
+    });
+  });
+});
+describe('Function', function() {
+  describe('Basic', function () {
+    it('should be able to handle a simple function', function () {
+        let tokens = scanner("def basic()\n\treturn 5", null);
+
+        assert.deepStrictEqual("def", tokens[0].type);
+        assert.deepStrictEqual("bareword", tokens[1].type);
+        assert.deepStrictEqual("basic", tokens[1].text);
+        assert.deepStrictEqual("openParen", tokens[2].type);
+        assert.deepStrictEqual("closeParen", tokens[3].type);
+        assert.deepStrictEqual("newline", tokens[4].type);
+        assert.deepStrictEqual("indent", tokens[5].type);
+        assert.deepStrictEqual("return", tokens[6].type);
+        assert.deepStrictEqual("intlit", tokens[7].type);
+        assert.deepStrictEqual("5", tokens[7].text);
+
+        let tree = parser(tokens, null);
+
+        let def = tree.block[0];
+        assert.deepStrictEqual("definition", def.type);
+        assert.deepStrictEqual("bareword", def.name.type);
+        assert.deepStrictEqual("basic", def.name.text);
+
+        let ret = def.body[0];
+        assert.deepStrictEqual("return", ret.type);
+        assert.deepStrictEqual("intlit", ret.value.type);
+        assert.deepStrictEqual("5", ret.value.text);
     });
   });
 });
